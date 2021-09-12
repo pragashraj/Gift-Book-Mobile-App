ae4320fa8586535f3b53bc016c78714a4796e55c
pragashraj <rajpragashraj30@gmail.com>
2021-09-12 18:14:06 +0530

    src structure

---
+++ src/screens/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class SignIn extends Component {
+    render() {
+        return (
+            <View>
+                <Text> SignIn </Text>
+            </View>
+        )
+    }
+}
+
+export default SignIn
---
+++ src/screens/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class SignUp extends Component {
+    render() {
+        return (
+            <View>
+                <Text> SignUp </Text>
+            </View>
+        )
+    }
+}
+
+export default SignUp
