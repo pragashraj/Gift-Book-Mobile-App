e4c98667b15e7b64d8a9897f83adae4d11f796a4
pragashraj <rajpragashraj30@gmail.com>
2021-10-06 18:40:15 +0530

    feat: payments -> datefilter

--- src/screens/Payments/MyPayments.js
+++ src/screens/Payments/MyPayments.js
@@ -1,27 +1,101 @@
 import React, { Component } from 'react'
-import { Text, View, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import { Text, View, SafeAreaView, ScrollView, TouchableOpacity, Image } from 'react-native'
 
 import TopBar from '../../components/TopBar'
 import Loading from '../../components/Loading'
+import DatePicker from '../../components/DatePicker'
 
 import {styles} from './styles'
+import calendar from '../../assets/images/icons/calendar.png'
+import close from '../../assets/images/icons/close.png'
 
 class MyPayments extends Component {
     state = {
-        loading: false
+        loading: false,
+        startDate: new Date(),
+        endDate: new Date(),
+        dateTag: "",
+        openDatePicker: false,
+    }
+
+    handleDateOnChange = (value) => {
+        const date = value.nativeEvent.timestamp
+        if (this.state.dateTag === "Start") {
+            this.setState({ startDate: date, openDatePicker: false, dateTag: "" })
+        }
+        else {
+            this.setState({ endDate: date, openDatePicker: false, dateTag: "" })
+        }
+    }
+
+    handleDateOnClick = (tag) => {
+        this.setState({ dateTag: tag, openDatePicker: true })
+    }
+
+    handleDateClearOnPress = () => {
+        this.setState({ startDate: new Date(), endDate: new Date() })
+    }
+
+    renderDate = (text) => {
+        return (
+            <TouchableOpacity style = {styles.date} onPress = {() => this.handleDateOnClick(text)}>
+                <Text style = {styles.dateText}>{text}</Text>
+                <Image style = {styles.calendarImg} source = {calendar}/>
+            </TouchableOpacity>
+        )
+    }
+
+    renderDatePicker = (dateTag, startDate, endDate) => {
+        return (
+            <DatePicker 
+                date = { dateTag === "Start" ? startDate : endDate} 
+                onChange = {this.handleDateOnChange}
+            />
+        )
+    }
+
+    renderDateFilter = () => {
+        const {startDate, endDate} = this.state
+        return (
+            <View style = {styles.filterBlock}>
+                <Text style = {styles.headerTitle}>Filter by date</Text>
+                <View style = {styles.filterItemRoot}>
+                    <View style = {styles.row}>
+                        { this.renderDate("Start") }
+                        { this.renderDate("End") }
+                    </View>
+                </View>
+                { startDate && endDate &&
+                    <View style = {styles.selectedDateRoot}>
+                        <View style = {styles.selectedDate}>
+                            <View style = {styles.row}>
+                                <Text>{startDate.toDateString()}</Text>
+                                <Text>{endDate.toDateString()}</Text>
+                            </View>
+                        </View>
+                        <View style = {styles.clearBtnRoot}>
+                            <TouchableOpacity onPress = {this.handleDateClearOnPress}>
+                                <Image style = {styles.icon} source = {close}/>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                }
+            </View>
+        )
     }
 
     render() {
-        const {loading} = this.state
+        const {loading, openDatePicker, dateTag, startDate, endDate} = this.state
         return (
             <SafeAreaView style = {styles.container}>
                 <TopBar title = "My Payments" navigation = {this.props.navigation}/>
                 <ScrollView style = {styles.scrollView} indicatorStyle = "white">
                     <View style = {styles.mainRoot}>
-
+                        { this.renderDateFilter() }
                     </View>
                 </ScrollView>
                 { loading && <Loading open = {loading}/> }
+                { openDatePicker && this.renderDatePicker(dateTag, startDate, endDate) }
             </SafeAreaView>
         )
     }
--- src/screens/Payments/styles.js
+++ src/screens/Payments/styles.js
@@ -19,4 +19,60 @@ export const styles = StyleSheet.create({
     mainRoot: {
         padding: 10
     },
+    filterBlock: {
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 10
+    },
+    headerTitle: {
+        color: "#000",
+        textTransform: "uppercase",
+        letterSpacing: 1.5,
+        fontWeight: "bold",
+        fontSize: 12
+    },
+    filterItemRoot: {
+        marginTop: 20
+    },
+    row: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between"
+    },
+    date: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 0.5,
+        padding: 5,
+        width: "45%",
+        justifyContent: "center"
+    },
+    dateText:{
+        textTransform: "uppercase",
+        marginRight: 5
+    },
+    calendarImg: {
+        width: 30,
+        height: 30
+    },
+    selectedDateRoot: {
+        marginTop: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    selectedDate: {
+        width: "85%",
+        marginLeft: "5%",
+    },
+    clearBtnRoot: {
+        width: "10%",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    icon: {
+        width: 25,
+        height: 25
+    }
 })
